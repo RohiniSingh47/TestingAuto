7a09cf155fc10666081a0a8ce5c39b4f80d241e8
singh <singh4rohini@gmail.com>
2023-06-08 11:16:49 +0100

    CRS-123 committing gitignore file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is my first github commit integrating with jenkins");
+        System.out.println("This is my first github commit integrating with jenkinssssss");
     }
 }
